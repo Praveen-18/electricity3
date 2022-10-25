b31ff6a36cc033eb1e678c6e426ef3ce1c3411b5
praveen <prav20ad036@rmkcet.ac.in>
2022-10-25 10:32:15 +0530

    Login authentication done :mechanical_arm:

--- ninja/views.py
+++ ninja/views.py
@@ -1,18 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from .models import user
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(request):
-    return render(request,'ninja/index.html')
+    return render(request,'ninja/index.html', {'user': request.user})
 
 def form(request):
-    return render(request,'ninja/form.html')
+    return render(request,'ninja/form.html' , {'user': request.user})
 
 def table(request):
-    return render(request,'ninja/table.html')
-
-def signin(request):
-    return render(request,'ninja/signin.html')
+    return render(request,'ninja/table.html' , {'user': request.user})
 
 def signup(request):
-    return render(request,'ninja/signup.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        print(name)
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        Curruser = user(name=name,email=email,phone=phone)
+        authUser = User.objects.create_user(name, email, password)
+        authUser.save()
+        Curruser.save()
+        return redirect('/index')
+    else:
+        return render(request,'ninja/signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        print(name, password)
+        curr_user = authenticate(username = name, password = password)
+        if curr_user is not None:
+            login(request , curr_user)
+            return redirect('/index')
+        else:
+            return render(request, 'ninja/signin.html')
+    else:
+        return render(request, 'ninja/signin.html')
